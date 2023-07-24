abd71982a9aa353f89ae88cc4deffd0cdda643a8
Samir Cavalcanti de Oliveira <107083863+DevSamirColiveira@users.noreply.github.com>
2023-07-24 19:47:33 -0300

    atualizar

--- assets/app/main.js
+++ assets/app/main.js
@@ -1,13 +1,13 @@
 let produtos = [];
-const endpointDaAPI = 'https://raw.githubusercontent.com/DevSamirColiveira/ecommerceAluraGeek/main/db.json';
+const endpointDaAPI = 'http://localhost:3000/produtos';
 
 getBuscarProdutosDaAPI();
 // funções assíncronas "async"
 async function getBuscarProdutosDaAPI (){
     const resposta = await fetch(endpointDaAPI)
-    livros = await resposta.json()
-    console.table(livros) //ver em tabela
-    let livrosComDesconto = aplicarDesconto(livros)
+    produtos = await resposta.json()
+    console.table(produtos) //ver em tabela
+    // let livrosComDesconto = aplicarDesconto(livros)
 
-    exibirOsLivrosNaTela(livrosComDesconto)
+    // exibirOsLivrosNaTela(livrosComDesconto)
 }
