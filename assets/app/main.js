c7e6dfe99ede555fcb4854e624cbee0864bf4fb9
Samir Cavalcanti de Oliveira <107083863+DevSamirColiveira@users.noreply.github.com>
2023-07-29 22:01:25 -0300

    banco

--- assets/app/main.js
+++ assets/app/main.js
@@ -3,12 +3,27 @@ const endpointDaAPI = 'https://raw.githubusercontent.com/DevSamirColiveira/ecomm
 // const endpointDaAPI = 'http://localhost:3000/produtos';
 
 getBuscarProdutosDaAPI();
-// funções assíncronas "async"
+const elementoParaInserirProdutos = document.getElementById('starwars')
+
+
 async function getBuscarProdutosDaAPI (){
     const resposta = await fetch(endpointDaAPI)
     produtos = await resposta.json()
-    console.table(produtos) //ver em tabela
-    // let livrosComDesconto = aplicarDesconto(livros)
+    console.table(produtos)
+    exibirOsProdutosNaTela(produtos)
+}
 
-    // exibirOsLivrosNaTela(livrosComDesconto)
+function exibirOsProdutosNaTela(listaDeProdutos) {
+    listaDeProdutos.forEach(produto => {
+        elementoParaInserirProdutos.innerHTML += `
+        <div class="Oproduto">
+          <img src="${produto.imageUrl}" alt="" class="imagem__produto">
+          <h2 class="nome__produto">
+            <a href="./pages/produto.html">Stormtrooper</a>
+          </h2>
+          <p class="preco__produto">R$ 100,00</p>
+          <a href="/" class="carrinho__produto">Comprar</a>
+        </div>
+        `
+    });
 }
